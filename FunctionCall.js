dcd354482bde97e39d03675f2e3e16b4d118842b
Malham30 <105686462+Malham30@users.noreply.github.com>
2023-11-07 07:26:40 -0600

    Add files via upload

---
+++ FunctionCall.js
@@ -0,0 +1,26 @@
+function play() {
+    //These lines bellow give value to the different variables created
+    var die1 = Math.ceil(Math.random() * 6);
+    var die2 = Math.ceil(Math.random() * 6);
+    var sum = die1 + die2;
+    var result = (sum)/2.0;
+    
+    document.getElementById("die1Res").innerHTML = "die1 = " + die1;
+    document.getElementById("die2Res").innerHTML = "die2 = " + die2;
+    document.getElementById("sumRes").innerHTML = "sum = " + sum;
+
+    //this creates an if else statement that helps tel wheter or not the person wins or loses
+    if (sum == 7 || sum == 11) {
+
+        document.getElementById("finalRes").innerHTML = "CRAPS - you lose";
+    }
+    else if (die1 == die2 && die1 % 2 == 0) {
+
+        document.getElementById("finalRes").innerHTML = "DOUBLES - you win";
+    } else {
+
+        document.getElementById("finalRes").innerHTML = "You did not win or lose, please try again.";
+    }
+alert(result);
+    document.getElementById("Average").innerHTML = "Average = " + result;
+}
